da0405afc18302246ab8643b1e4c8cb215a131b1
Dev <DEVKUMAWAT7151@gmail.com>
2026-08-08 00:10:41 +0530

    Java learning journal starting

---
+++ Questions Arrays/firstmissingpositive.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int firstMissingPositive(int[] arr) {
+        int i = 0;
+
+        // Cyclic sort
+        while (i < arr.length) {
+            int correct = arr[i] - 1;
+
+            if (arr[i] > 0 && arr[i] <= arr.length && arr[i] != arr[correct]) {
+                swap(arr, i, correct);
+            } else {
+                i++;
+            }
+        }
+
+        // Find first missing positive
+        for (int index = 0; index < arr.length; index++) {
+            if (arr[index] != index + 1) {
+                return index + 1;
+            }
+        }
+
+        return arr.length + 1;
+    }
+
+    void swap(int[] arr, int f, int s) {
+        int temp = arr[f];
+        arr[f] = arr[s];
+        arr[s] = temp;
+    }
+}
