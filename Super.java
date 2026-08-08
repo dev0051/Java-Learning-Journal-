3f28e0e75c1083b68f351b0c7c281854009e3c93
Dev <DEVKUMAWAT7151@gmail.com>
2026-08-08 22:15:13 +0530

    super()

---
+++ Super.java
@@ -0,0 +1,24 @@
+class Employee{
+
+    String name;
+
+    Employee(String name){
+        this.name= name;
+    }
+}
+class Developer extends Employee{
+    String language;
+
+    Developer(String name , String language){
+        super(name);
+        this.language= language;
+    }
+}
+public class Super{
+    public static void main(String[] args){
+        Developer dev= new Developer("dev","java");
+        System.out.println(dev.name);
+        System.out.println(dev.language);
+
+    }
+}
