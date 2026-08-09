dcbffea649ee2e1b619180e2916adcdeecdd969d
Dev <DEVKUMAWAT7151@gmail.com>
2026-08-09 19:57:14 +0530

    understanding the use of Anonymous Inner Class

---
+++ AnonymousInnerClass.java
@@ -0,0 +1,22 @@
+class A
+{
+    public void show()
+    {
+        System.out.println("in  a show ");
+    }
+}
+
+
+public class AnonymousInnerClass{
+    public static void main(String[] args){
+
+        A obj = new A()
+        {
+            public void show()
+            {
+                System.out.println("in a show");
+            }
+        };
+        obj.show();
+    }
+}
