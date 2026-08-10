3aee6042087e6ba4d3faba2b065978f01ce81ce9
Dev <DEVKUMAWAT7151@gmail.com>
2026-08-10 22:42:21 +0530

    Exception using throw

---
+++ ExceptionThrow.java
@@ -0,0 +1,25 @@
+public class ExceptionThrow {
+
+    public static void main(String[] args){
+        int i = 0;
+        int j = 0;
+
+        try
+        {
+            j=18/i;
+            if(j==0) throw new ArithmeticException("NO Zeroes Allowed....");
+        }
+        catch(ArithmeticException e)
+        {
+            j=18/i;
+            System.out.println("Default "+ e);
+        }
+        catch(Exception e)
+        {
+            System.out.println("Something wrong"+e);
+
+        }
+        System.out.println(j);
+        System.out.println("Good bye");
+    }
+}
