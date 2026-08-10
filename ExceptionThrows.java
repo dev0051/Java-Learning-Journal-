373817624b44335171be00d7b2987a3dc589075f
Dev <DEVKUMAWAT7151@gmail.com>
2026-08-10 22:49:47 +0530

    Exception using Throws

---
+++ ExceptionThrows.java
@@ -0,0 +1,30 @@
+class DevException extends Exception {
+    public DevException(String string) {
+        super(string);
+    }
+}
+
+class A {
+    public void show() throws ClassNotFoundException {
+        Class.forName("calc");
+    }
+}
+
+public class ExceptionThrows {
+
+    static {
+        System.out.println("class load");
+    }
+
+    public static void main(String[] args) {
+
+        A obj = new A();
+
+        try {
+            obj.show();
+        }
+        catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
