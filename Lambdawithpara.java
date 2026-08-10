5f2a1a88e121ea456b62c7a46ec02f05bc079202
Dev <DEVKUMAWAT7151@gmail.com>
2026-08-10 17:51:28 +0530

    Lambda expression with return statement

---
+++ Lambdawithpara.java
@@ -0,0 +1,16 @@
+FunctionalInterface
+interface A
+{
+    int add(int i, int j);
+}
+
+
+public class Lambdawithpara{
+    public static void main(String[] args) {
+
+        A obj=(i,j) -> i+j;
+
+        int result=obj.add(5, 4);
+        System.out.println(result);
+    }
+}
