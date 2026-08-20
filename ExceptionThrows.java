0df950f327201143cd4005f04abcbd6e748f92a5
Dev <DEVKUMAWAT7151@gmail.com>
2026-08-20 19:36:52 +0530

    Changes

--- ExceptionThrows.java
+++ ExceptionThrows.java
@@ -24,7 +24,7 @@ public class ExceptionThrows {
             obj.show();
         }
         catch (ClassNotFoundException e) { //hello
-            e.printStackTrace(); // hellooo
+            e.printStackTrace(); // hellooooo
         }
     }
 }
