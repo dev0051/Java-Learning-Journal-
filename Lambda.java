356f144aa686eb70441a8e711a69d2b3a0993150
devvvvvv <DEVKUMAWAT7151@GMAIL.COM>
2026-08-24 14:19:28 +0000

    docs: clarify lambda example

--- Lambda.java
+++ Lambda.java
@@ -6,7 +6,7 @@ public class Lambda {
 
     public static void main(String[] args){
 
-        A obj = ()  -> System.out.println("In show ");  // instead of writing the tarditional way of implementration just write lambda expression
+        A obj = ()  -> System.out.println("In show ");  // A lambda replaces the traditional implementation of the functional interface
         obj.show();
     }
 }
